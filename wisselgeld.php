043bf3d0739d2fbb7486f824630137239738297c
Stijn van_der_Made <2120804@talnet.nl>
2019-11-27 13:22:43 +0100

    Uitwerking van de exercise

---
+++ wisselgeld.php
@@ -0,0 +1,19 @@
+<?php
+$input = $argv[1];
+$input = round($input/0.05)*0.05;
+$wisselgeld = array(50, 20, 10, 5, 2 , 1 , 0.5, 0.2, 0.1, 0.05);
+foreach ($wisselgeld as $moela) {
+    $amount = floor($input/$moela);
+    if ($amount != 0) {
+        $input -=($amount * $moela);
+        if ($moela >= 1){ 
+            echo "\n $amount x  $moela euro ";
+        }
+        else{
+            $moela*=100;
+            echo "\n $amount x $moela cent";
+        }
+    }
+}
+
+?>
